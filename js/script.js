480fb00eb05d621f5a24f7d2329c2664e05389bd
Jaga <jagblst@gmail.com>
2022-04-19 13:12:39 +0300

    1_st commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// alert("Hello");
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("are you 18?", "18");
+// console.log(answer + 5);
+
+const answers = [];
+
+answers[0] = prompt('What is your name?', '');
+answers[1] = prompt('What is your surname?', '');
+answers[2] = prompt('How old are you?', '');
+
+document.write(answers);
