0c4453c40b4b1c691529bc2a5dcb2936af34bdf6
Jaga <jagblst@gmail.com>
2022-04-19 20:30:02 +0300

    условия

---
+++ script.js
@@ -0,0 +1,36 @@
+"use strict";
+
+if (4 == 9) {
+    console.log('Ok!');
+} else {
+    console.log('Error');
+}
+
+const num = 50;
+
+if (num < 49) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log('Many');
+} else {
+    console.log('Ok!'); 
+}
+
+(num === 50) ? console.log('Ok') : console.log('Error!');
+
+const num = 50;
+
+switch (num) {
+    case 49:
+        console.log('no!');
+        break;
+    case 100:
+        console.log('no!');
+        break;
+    case 51:
+        console.log('Yes!');
+        break;   
+    default:
+        console.log('not at time');
+        break;         
+}
