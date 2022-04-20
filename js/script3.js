c73088204a48ee38e3d05f3289ac7434aab990bb
Jaga <jagblst@gmail.com>
2022-04-20 21:33:51 +0300

    Методы и свойства строк и чисел

---
+++ js/script3.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const str = "teSt";
+
+
+// console.log(str[2] = 'd');
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str);
+
+
+const fruit = "Some fruit";
+console.log(fruit.indexOf("q"));
+
+const logg = "Hello world";
+console.log(logg.slice(6, 11));
+
+console.log(logg.substring(6, 11));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test));
