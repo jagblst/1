99cbdea2e94a4fa625771bbeffa051eb25795ac9
Jaga <jagblst@gmail.com>
2022-05-24 18:33:11 +0300

    Модификации модального окна

--- food/js/script.js
+++ food/js/script.js
@@ -108,14 +108,18 @@ window.addEventListener('DOMContentLoaded', () => {
           modal = document.querySelector('.modal'),
           modalCloseBtn = document.querySelector('[data-close]'); 
    
+    function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        // modal.classList.toggle('show');
+        document.body.style.overflow ='hidden';
+        clearInterval(modalTimerId);
+    }
+
     modalTrigger.forEach(btn => {
-        btn.addEventListener('click', () => {
-            modal.classList.add('show');
-            modal.classList.remove('hide');
-            // modal.classList.toggle('show');
-            document.body.style.overflow ='hidden';
-        });
+        btn.addEventListener('click', openModal);
     });     
+
 
     function closeModal() {
         modal.classList.add('hide');
@@ -136,4 +140,17 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }
     });
+
+    const modalTimerId = setTimeout(openModal, 3000);
+
+    function showModalByScroll(){
+        if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
+        
+    
 });
